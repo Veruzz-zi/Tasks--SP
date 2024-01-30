6526fb3606f6d683c0e22ac1d62ac5c43c51bd3b
Veruzz-zi <158046441+Veruzz-zi@users.noreply.github.com>
2024-01-30 14:36:23 +0100

    Created two Webpages

--- tasks/main/urls.py
+++ tasks/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name="main")
+    path('MainForm/', views.mainForm, name="mainForm"),
+    path('MainList/', views.mainList, name="mainList")
 ]
--- tasks/main/views.py
+++ tasks/main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-def main(request):
-    return render(request, 'main/main.html')
+def mainForm(request):
+    return render(request, 'main/mainForm.html')
+
+
+def mainList(request):
+    return render(request, 'main/mainList.html')
